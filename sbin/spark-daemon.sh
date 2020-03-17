7b0c9fd360a179b05a67229b7d06e6d397dac41c
mlamp <xuxiaoguang@mininglamp.com>
2020-03-17 13:53:41 +0800

    read code

--- sbin/spark-daemon.sh
+++ sbin/spark-daemon.sh
@@ -32,26 +32,27 @@
 
 usage="Usage: spark-daemon.sh [--config <conf-dir>] (start|stop|submit|status) <spark-command> <spark-instance-number> <args...>"
 
-# if no args specified, show usage
+# if no args specified, show usage  # 如果没有提供参数，则打印usage使用说明并中断脚本
 if [ $# -le 1 ]; then
   echo $usage
   exit 1
 fi
-
+# 判断是否配置了SPARK_HOME，如果没有则手动设置
 if [ -z "${SPARK_HOME}" ]; then
   export SPARK_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 fi
-
+# 通过.加上文件名临时执行spark-config.sh脚本
+## 设置SPARK_HOME、SPARK_CONF_DIR以及python的一些环境变量
 . "${SPARK_HOME}/sbin/spark-config.sh"
 
 # get arguments
 
 # Check if --config is passed as an argument. It is an optional parameter.
 # Exit if the argument is not a directory.
-
+# 检查参数中是否包含--config，如果该参数的值不为目录则退出
 if [ "$1" == "--config" ]
 then
-  shift
+  shift # 移除第一个参数
   conf_dir="$1"
   if [ ! -d "$conf_dir" ]
   then
@@ -63,7 +64,7 @@ then
   fi
   shift
 fi
-
+# 分别对option、command、instance进行赋值
 option=$1
 shift
 command=$1
@@ -87,17 +88,18 @@ spark_rotate_log ()
 	mv "$log" "$log.$num";
     fi
 }
-
+# 通过.加上文件名临时执行load-spark-env.sh脚本
+## 设置SPARK_HOME、SPARK_SCALA_VERSION环境变量
 . "${SPARK_HOME}/bin/load-spark-env.sh"
 
 if [ "$SPARK_IDENT_STRING" = "" ]; then
   export SPARK_IDENT_STRING="$USER"
 fi
 
-
+# 设置SPARK_PRINT_LAUNCH_COMMAND环境变量
 export SPARK_PRINT_LAUNCH_COMMAND="1"
 
-# get log directory
+# get log directory  # 日志相关
 if [ "$SPARK_LOG_DIR" = "" ]; then
   export SPARK_LOG_DIR="${SPARK_HOME}/logs"
 fi
@@ -109,28 +111,32 @@ if [ "${TEST_LOG_DIR}" = "0" ]; then
 else
   chown "$SPARK_IDENT_STRING" "$SPARK_LOG_DIR"
 fi
-
+# 设置spark进程目录
 if [ "$SPARK_PID_DIR" = "" ]; then
   SPARK_PID_DIR=/tmp
 fi
 
-# some variables
+# some variables  # 定义log和pid变量
 log="$SPARK_LOG_DIR/spark-$SPARK_IDENT_STRING-$command-$instance-$HOSTNAME.out"
 pid="$SPARK_PID_DIR/spark-$SPARK_IDENT_STRING-$command-$instance.pid"
 
-# Set default scheduling priority
+# Set default scheduling priority  # 设置默认的调度优先级
 if [ "$SPARK_NICENESS" = "" ]; then
     export SPARK_NICENESS=0
 fi
-
+ # 该方法
 execute_command() {
+  # 看不懂+set是啥意思...不过debug的时候会进这个条件判断
   if [ -z ${SPARK_NO_DAEMONIZE+set} ]; then
+      # 后台执行脚本命令，此处是获取传入该方法的所有参数，进行运行
       nohup -- "$@" >> $log 2>&1 < /dev/null &
+       # 获取最新的后台运行的pid，即上条命令所占用的pid
       newpid="$!"
 
       echo "$newpid" > "$pid"
 
       # Poll for up to 5 seconds for the java process to start
+      # 共等待5秒来等待上述程序的运行
       for i in {1..10}
       do
         if [[ $(ps -p "$newpid" -o comm=) =~ "java" ]]; then
@@ -139,8 +145,9 @@ execute_command() {
         sleep 0.5
       done
 
-      sleep 2
+      sleep 2  # 再等待2秒
       # Check if the process has died; in that case we'll tail the log so the user can see
+      # 当程序正常运行时，打印日志
       if [[ ! $(ps -p "$newpid" -o comm=) =~ "java" ]]; then
         echo "failed to launch: $@"
         tail -2 "$log" | sed 's/^/  /'
@@ -150,13 +157,13 @@ execute_command() {
       "$@"
   fi
 }
-
+# 该方法主要判断是否能继续进行，并做一些预处理
 run_command() {
-  mode="$1"
+  mode="$1"   # 获取第一个参数赋值给mode
   shift
-
+  # 创建进程目录
   mkdir -p "$SPARK_PID_DIR"
-
+  # 判断进程文件是否存在，如果存在则给出提示：进程已存在，请先停止
   if [ -f "$pid" ]; then
     TARGET_ID="$(cat "$pid")"
     if [[ $(ps -p "$TARGET_ID" -o comm=) =~ "java" ]]; then
@@ -164,7 +171,7 @@ run_command() {
       exit 1
     fi
   fi
-
+  # 如果spark_master不为空，则在masters(master高可用的情况)中同步删除部分文件
   if [ "$SPARK_MASTER" != "" ]; then
     echo rsync from "$SPARK_MASTER"
     rsync -a -e ssh --delete --exclude=.svn --exclude='logs/*' --exclude='contrib/hod/logs/*' "$SPARK_MASTER/" "${SPARK_HOME}"
@@ -172,13 +179,15 @@ run_command() {
 
   spark_rotate_log "$log"
   echo "starting $command, logging to $log"
-
+ # 使用mode变量进行匹配
   case "$mode" in
     (class)
+      # 当变量为class时调用execute_command，传入参数
       execute_command nice -n "$SPARK_NICENESS" "${SPARK_HOME}"/bin/spark-class "$command" "$@"
       ;;
 
     (submit)
+      # 当变量为submit时调用execute_command，传入参数
       execute_command nice -n "$SPARK_NICENESS" bash "${SPARK_HOME}"/bin/spark-submit --class "$command" "$@"
       ;;
 
@@ -189,17 +198,17 @@ run_command() {
   esac
 
 }
-
+# 使用option变量进行匹配
 case $option in
-
+   # 如果变量为submit则执行run_command方法，并将submit作为第一个参数传入
   (submit)
     run_command submit "$@"
     ;;
-
+  # 如果变量为start则执行run_command方法，并将start作为第一个参数传入
   (start)
     run_command class "$@"
     ;;
-
+   # 如果变量为stop，则执行kill命令，并删除进程文件
   (stop)
 
     if [ -f $pid ]; then
@@ -214,7 +223,7 @@ case $option in
       echo "no $command to stop"
     fi
     ;;
-
+  # 如果变量为status，则打印状态信息
   (status)
 
     if [ -f $pid ]; then
@@ -231,7 +240,7 @@ case $option in
       exit 2
     fi
     ;;
-
+  # 如果变量未匹配上上述情况，则打印脚本使用方法
   (*)
     echo $usage
     exit 1
@@ -239,4 +248,7 @@ case $option in
 
 esac
 
-
+#debug spark-daemon.sh脚本
+#sh -x /usr/local/spark/sbin/spark-daemon.sh start org.apache.spark.deploy.master.Master 1 --host s101 --port 7077 --webui-port 8080
+# spark-daemon.sh调用spark-class
+#sh -x /usr/local/spark/bin/spark-class org.apache.spark.deploy.master.Master --host s101 --port 7077 --webui-port 8080
--- sbin/start-master.sh
+++ sbin/start-master.sh
@@ -18,15 +18,17 @@
 #
 
 # Starts the master on the machine this script is executed on.
-
+# 判断是否配置了SPARK_HOME，如果没有设置则先通过$0获取当前脚本的文件名
+# 再通过dirname获取sbin目录，再cd回上级目录，pwd获取绝对路径，以此设置SPARK_HOME
 if [ -z "${SPARK_HOME}" ]; then
   export SPARK_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 fi
 
 # NOTE: This exact class name is matched downstream by SparkSubmit.
 # Any changes need to be reflected there.
+# 定义CLASS变量
 CLASS="org.apache.spark.deploy.master.Master"
-
+# 判断是否以--help或者-h结尾，如果是的话则打印出脚本使用方法
 if [[ "$@" = *--help ]] || [[ "$@" = *-h ]]; then
   echo "Usage: ./sbin/start-master.sh [options]"
   pattern="Usage:"
@@ -36,19 +38,24 @@ if [[ "$@" = *--help ]] || [[ "$@" = *-h ]]; then
   "${SPARK_HOME}"/bin/spark-class $CLASS --help 2>&1 | grep -v "$pattern" 1>&2
   exit 1
 fi
-
+# 通过$@取出所有参数，并将值赋值给ORIGINAL_ARGS
 ORIGINAL_ARGS="$@"
 
+# 通过 .加上文件名临时执行spark-config.sh脚本
+## 设置SPARK_HOME、SPARK_CONF_DIR以及python的一些环境变量
 . "${SPARK_HOME}/sbin/spark-config.sh"
 
+# 通过.加上文件名临时执行load-spark-env.sh脚本
+## 设置SPARK_HOME、SPARK_SCALA_VERSION环境变量
 . "${SPARK_HOME}/bin/load-spark-env.sh"
 
+# 设置master的通信端口
 if [ "$SPARK_MASTER_PORT" = "" ]; then
   SPARK_MASTER_PORT=7077
 fi
-
+# 设置master的主机地址
 if [ "$SPARK_MASTER_HOST" = "" ]; then
-  case `uname` in
+  case `uname` in     # 不同的操作系统使用不同的命令进行查看
       (SunOS)
 	  SPARK_MASTER_HOST="`/usr/sbin/check-hostname | awk '{print $NF}'`"
 	  ;;
@@ -57,11 +64,16 @@ if [ "$SPARK_MASTER_HOST" = "" ]; then
 	  ;;
   esac
 fi
-
+# 设置master ui端口
 if [ "$SPARK_MASTER_WEBUI_PORT" = "" ]; then
   SPARK_MASTER_WEBUI_PORT=8080
 fi
-
+# 调用spark-daemon.sh脚本，传入master启动所需参数
 "${SPARK_HOME}/sbin"/spark-daemon.sh start $CLASS 1 \
   --host $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT \
   $ORIGINAL_ARGS
+
+#如果需要调试
+## 1.1. 执行 sh -x start-master.sh
+#. start-master.sh调用spark-daemon.sh
+#sh -x /usr/local/spark/sbin/spark-daemon.sh start org.apache.spark.deploy.master.Master 1 --host s101 --port 7077 --webui-port 8080
